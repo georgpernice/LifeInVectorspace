0cccb17bcffeda839cd80b75a97a5f852fb8fc14
georg <georgpernice@gmail.com>
2024-11-13 21:17:33 +0100

    add more func to day vec

--- main.py
+++ main.py
@@ -22,6 +22,35 @@ class DayVector:
         self.dim_values.append(value)
         self.dim += 1
 
+    def reduce_dim(self, new_dim):
+        """Reduce the dimension of the day vector by removing the least important deeds."""
+        if new_dim >= self.dim:
+            return
+        while self.dim > new_dim:
+            min_value = min(self.dim_values)
+            min_index = self.dim_values.index(min_value)
+            self.dim_labels.pop(min_index)
+            self.dim_values.pop(min_index)
+            self.dim -= 1
+
     def print(self):
+        output = ""
+        output += "Day vector: transpose("
         for i in range(self.dim):
-            print(self.dim_labels[i], self.dim_values[i])
+            output += str(self.dim_values[i]) + ", "
+        print(
+            output
+            + ") "
+            + "element of "
+            + str([i for i in self.dim_labels])
+            + " basis."
+        )
+
+
+monday_vector = DayVector()
+for key in monday:
+    monday_vector.add_deed(key, monday[key])
+
+monday_vector.print()
+monday_vector.reduce_dim(3)
+monday_vector.print()
